d0200c5b6884062c471139bc8c81e6d0ab4dbf49
ImanGhader <emanghader@gmail.com>
2015-02-01 22:15:27 -0500

    added footer text, removed icons

--- js/work.js
+++ js/work.js
@@ -1,15 +1,11 @@
 var $win = $(window);
 var $iawBanner = $('.iaw-banner');
 var iawBannerOffset = $iawBanner.offset();
-var $salamBanner = $('.salam-banner');
-var salamBannerOffset = $salamBanner.offset();
 
 	$win.on('scroll', function () {
 		
 		var scrollPos = $win.scrollTop();
 		var iawPos = (scrollPos - iawBannerOffset.top) / 2;
-		var salamPos = (scrollPos - salamBannerOffset.top) / 2;
 
 		$iawBanner.css('background-position', 'center ' + iawPos + 'px');
-		$salamBanner.css('background-position', 'center ' + salamPos + 'px');
 	});
